ba42e9d4dce0111d5010e1b280da9609ffc05eed
yuuki008 <s22ah261@gmail.com>
2024-06-14 09:36:05 +0900

    commentary 追加

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -84,6 +84,9 @@ return packer.startup(function(use)
 
 	use({ "windwp/nvim-ts-autotag" })
 
+  -- Commentary
+	use({ "tpope/vim-commentary" })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
